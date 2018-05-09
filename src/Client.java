46d62d8946f27ae9ca33f47dbdedbc386febd137
Jared Simmons <simmons.jared76@gmail.com>
2018-05-09 12:10:00 -0400

    Removed ACK code attempt, added a README file

--- src/Client.java
+++ src/Client.java
@@ -12,20 +12,11 @@ public class Client {
             Socket connect = new Socket(server, port);
             connect.setTcpNoDelay(true);
             connect.setSoTimeout(0);
-            InputStream in = connect.getInputStream();
             OutputStream out = connect.getOutputStream();
-            DataInputStream listen = new DataInputStream(in);
             DataOutputStream send = new DataOutputStream(out);
             send.write(Base64.getEncoder().encode(message.getBytes()));
             send.flush();
-            switch(listen.readInt()) {
-                case 200:
-                    System.out.println("Connection successful.");
-                    break;
-                default:
-                    System.out.println("Abnormal termination condition.");
-                    break;
-            }
+            connect.close();
         } else {
             System.err.println("Error: Message is either blank or null.");
         }
--- src/ServerTest.kt
+++ src/ServerTest.kt
@@ -1,8 +1,9 @@
 import java.io.*
 import java.net.*
 import java.util.*
+import kotlin.system.*
 
-class ServerTest constructor(public val port: Int): Runnable {
+class ServerTest constructor(val port: Int): Runnable {
     override fun run() {
         val listen = ServerSocket(port)
         println("Waiting for connection on port $port...")
@@ -13,15 +14,19 @@ class ServerTest constructor(public val port: Int): Runnable {
         try {
             val sender = DataOutputStream(BufferedOutputStream(client.getOutputStream()))
             val receiver = DataInputStream(BufferedInputStream(client.getInputStream()))
-            var string = receiver.readBytes()
+            var string: ByteArray
+            string = receiver.readBytes()
             println("Data received.")
             val b64 = Base64.getDecoder()
             println("Data is:")
             println(String(b64.decode(string)))
             println("Sending acknowledgement.")
             sender.write(200)
+            sender.close()
         } catch(e: IOException) {
             System.err.println("Error: $e")
+            e.printStackTrace()
+            exitProcess(1)
         }
 
     }
